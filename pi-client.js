ce79a50e5e9d52ab335666a41d7810336511ee69
Brian Farnsworth <farnswor@adobe.com>
2017-06-23 21:01:40 -0600

    Got the LED flashing remotely! Cleaning things up

--- pi-client.js
+++ pi-client.js
@@ -1,6 +1,25 @@
-const io = require('socket.io-client')
-var socket = io('https://quiet-reaches-27634.herokuapp.com/');
-// var socket = io('http://localhost:8080');
-socket.on('time', function(timeString) {
-  console.log(timeString);
+// after changing this file, use scp to copy to the pi:
+//scp ~/life/Side-Projects/pi-sockets/pi-client.js pi@10.210.56.230:~/Projects/pi-sockets/script.js
+
+const onoff = require('onoff');
+const io = require('socket.io-client');
+
+var Gpio = onoff.Gpio;
+var led = new Gpio(4, 'out');
+var socket = io('https://quiet-reaches-27634.herokuapp.com/')
+
+socket.on('toggleLED', function(){
+	console.log('button pressed!');
+	var value = (led.readSync() + 1) % 2;
+	led.write(value, function() {
+		console.log(value);
+	});
 });
+
+process.on('SIGINT', function() {
+	clearInterval(interval);
+	led.writeSync(0);
+	led.unexport();
+	console.log('done')
+	process.exit();
+})
--- server.js
+++ server.js
@@ -1,32 +1,36 @@
 'use strict';
 
-console.log('server.js::3 :: ');
-
 const express = require('express');
 const socketIO = require('socket.io');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
 const INDEX = path.join(__dirname, 'index.html');
-
 const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
-
+	.use((req, res) => res.sendFile(INDEX) )
+	.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 const io = socketIO(server);
 
+
 var sockets = [];
-
 io.on('connection', (socket) => {
-  console.log('Client connected');
-  sockets.push(socket);
-  socket.on('toggleLED', function(){
-      console.log('toggleLED')
-      sockets.forEach(function(s) {
-         s.emit('toggleLED');
-      });
-  });
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+	console.log('Client connected');
+	
+	//add to list of sockets
+	sockets.push(socket);
+	
+	//listen for LED changes
+	socket.on('toggleLED', function(){
+		console.log('toggleLED')
+		sockets.forEach(function(s) {
+			s.emit('toggleLED');
+		});
+	});
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
+	socket.on('disconnect', () => {
+		console.log('Client disconnected');
+
+		//remove socket from list
+		sockets = sockets.filter(s => s !== socket);
+	});
+});
